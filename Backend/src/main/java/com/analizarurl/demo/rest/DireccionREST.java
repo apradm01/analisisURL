20889f82373e72ff3fd79163e3247493efbf0e11
apradm01 <apradm01@estudiantes.unileon.es>
2023-10-21 16:58:14 +0200

    backend: obtencion del numero de los distintos titulos y numero de imagenes

--- Backend/src/main/java/com/analizarurl/demo/rest/DireccionREST.java
+++ Backend/src/main/java/com/analizarurl/demo/rest/DireccionREST.java
@@ -44,13 +44,43 @@ public class DireccionREST {
         Direccion direccion = new Direccion();
         String rawText;
         int counter;
+        int h1 = 0;
+        int h2 = 0;
+        int h3 = 0;
+        int h4 = 0;
+        int images = 0;
 
         try {
             HtmlPage page = webClient.getPage(url);
             direccion.setUrl(url);
             direccion.setTitle(page.getTitleText());
             rawText = page.asNormalizedText();
-            
+
+            for (counter = 0; counter < rawText.length(); counter++){
+                switch(rawText.charAt(counter)){
+                    case 'h':
+                        if (rawText.charAt(counter+1) == '1'){
+                            h1++;
+                        } else if (rawText.charAt(counter+1) == '2'){
+                            h2++;
+                        } else if (rawText.charAt(counter+1) == '3'){
+                            h3++;
+                        } else if (rawText.charAt(counter+1) == '4'){
+                            h4++;
+                        }
+                        break;
+                    case 'i':
+                        if (rawText.charAt(counter+1) == 'm' && rawText.charAt(counter+2) == 'g'){
+                            images++;
+                        }
+                }
+            }
+            //Los titulos h1,h2,h3 y h4 aparecen tanto para abrir como cerrar un texto
+            //El contador final de los mismos es por ende la mitad de lo que hemos contado
+            h1 = h1/2;
+            h2 = h2/2;
+            h3 = h3/2;
+            h4 = h4/2;
 
 
         } catch (IOException e) {
